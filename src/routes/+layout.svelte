2c50f0f613290e94b87676e08885e13a4b991d2b
Rupsnigdha <rupsnigdhakashyap@gmail.com>
2025-04-28 22:32:50 +0530

    initial commit, do not read subsequent commit messages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="relative">
+	<Navbar />
+	{@render children()}
+</div>
+<div class="fixed -top-[30%] -right-[10%] w-[600px] h-[600px] rounded-full bg-purple-500/10 blur-[120px]"></div>
+<div class="fixed top-[60%] -left-[10%] w-[400px] h-[400px] rounded-full bg-blue-500/10 blur-[100px]"></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<section class="container mx-auto flex h-screen items-center px-4">
+	<div class="flex flex-col text-white">
+		<p class="">Hey, I'm</p>
+		<h2 class="font-lora mb-4 text-4xl">Portia</h2>
+		<p class="max-w-sm text-slate-600">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga asperiores, optio in illo
+			dolorum modi quam excepturi fugit ipsa vero dolor debitis incidunt recusandae, nisi
+			consectetur dolorem, nostrum dicta. Facilis.
+		</p>
+		<a href="#showcase" class="block bg-slate-200 text-[#000910] self-start font-lora px-8 py-2 relative mt-4">View my works!
+            <div class="h-full w-full bg-black absolute top-1 -right-1 -z-10"></div>
+        </a>
+	</div>
+</section>
+
+<section id="showcase" class="container mx-auto flex h-screen items-center px-4">
+    <h2>Portfolio</h2>
+</section>
